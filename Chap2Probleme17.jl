5fc8adaf6a5a125b1766fcf42e1c96b508e8077f
Jacobmercier <jacob.mercier.2@ulaval.ca>
2025-09-25 15:07:15 -0400

    Update Chap2Probleme17.jl

--- Chap2Probleme17.jl
+++ Chap2Probleme17.jl
@@ -6,27 +6,25 @@ using HypothesisTests
 
 # Algorithme Zombie Gap pour simuler des temps d'arrivée approximativement exponentiels
 
-function zombie_gap_algorithm(w::Float64, n_evenements::Int)
-    temps_arrivee = zeros(n_evenements)
-    temps_interarrivee = zeros(n_evenements)
+function zombie_gap(w, n)
+    arrivee = Int[]
     
-    temps_actuel = 0
-    
-    for i in 1:n_evenements
+    for i in 1:n
         compteur = 0
         while true
             compteur += 1
             U = rand()  # U ∼ U(0, 1)
             if U < w
+                push!(arrivee, compteur)
                 break
             end
         end
-        temps_actuel += compteur
-        temps_arrivee[i] = temps_actuel
-        temps_interarrivee[i] = compteur
     end
-    
-    return temps_arrivee, temps_interarrivee
+    return arrivee
 end
 
-print('a')
+w = 0.1
+n = 100000
+temps = zombie_gap(w, n)
+temps_moyen = mean(temps)
+print("temps_moyen : ", temps_moyen, " (devrait être ≈ ", 1/w, ")")
